5cef834d5ce592d49fbcffffbdd3368a9399d899
Joseph Arias <arias.joseph.a@gmail.com>
2018-12-11 18:52:45 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,51 @@ class App extends React.Component {
     }
   }
 
+  changeFilter = (word) => {
+    this.setState({
+      filters: {
+        type: word
+      }
+    })
+  }
+
+  adoptPet = (id) => {
+    let currentPets = [...this.state.pets]
+    let petId = id
+    console.log(petId)
+    let findPet = currentPets.find(pet => {
+      return pet.id == petId
+    })
+    findPet.isAdopted = true;
+ 
+
+    this.setState({
+      pets: currentPets
+      
+    })
+  }
+
+  fetchAPI = () => {
+    let petValue = this.state.filters.type
+    if ( petValue === 'all') {
+      fetch('/api/pets')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            pets: data
+          })
+        })
+    } else {
+      fetch(`/api/pets?type=${petValue}`)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            pets: data
+          })
+        })
+    }
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +69,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPetsClick={this.fetchAPI} onChangeType={this.changeFilter} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.adoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
